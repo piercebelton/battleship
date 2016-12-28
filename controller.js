ebf6ebaf4be8d9a9f12c400f81b82c836f563272
piercebelton <piercebelton@gmail.com>
2016-12-27 16:12:55 -0800

    first commit

---
+++ controller.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+  // Make the view board
+  makeViewBoard();
+  // Make the model board
+  makeModelBoard();
+
+  placeShipOfLength(5);
+  placeShipOfLengthAndOrientation(4, true);
+  placeShipOfLengthAndOrientation(4, false);
+  placeShipOfLengthAndOrientation(3, true);
+  placeShipOfLengthAndOrientation(3, false);
+  placeShipOfLengthAndOrientation(2, true);
+  placeShipOfLengthAndOrientation(2, false);
+  placeShipOfLength(1);
+
+  //This function operates when a cell is clicked
+  $("td").on("click",function() {
+    //This checks if a ship is in cell, and changes color to red if so
+    if(findOnBoard(parseInt($(this).attr("id"))) === SHIP) {
+      $(this).addClass("hit");
+      //adds to hit counter and checks if we've won
+      if (addHitAndCheckWin()) {
+        $("td").off("click");
+        $("#win_lose").text("YOU WIN!");
+      }
+    } else {
+      //This changes cell to gray
+      $(this).addClass("miss");
+    }
+
+    // When a user clicks the cell, the number of torpedoes also goes up
+    // by 1, we check if we've lost
+    if (addTorpedoAndCheckLose() && !addHitAndCheckWin()) {
+      $("td").off("click");
+      $("#win_lose").text("You lose, LOSER!");
+      showShips(); // turn all cells with ships in them red
+    }
+    //show number of torpedoes used
+    $("#torpedoes").text("Torpedoes remaining: " + (maxTorpedoes - numTorpedoes));
+
+    // Now we turn the cell off so we don't "waste" torpedoes
+    $(this).off("click");
+  });
+
+});
+
+// Loops through each cell in view and checks its value on the model board;
+// if the element on the model board has a ship, we turn the cell red on the
+// view
+function showShips() {
+  for (var i = 0; i < 10; i++) {
+    for(var j = 0; j < 10; j++) {
+      var idStr = "#" + i + j;
+      if(findOnBoard(parseInt($(idStr).attr("id"))) === SHIP) {
+        $(idStr).addClass("hit");
+      }
+    }
+  }
+}
+
+
+
+// This function uses 2 for loops to create the board on the view.
+function makeViewBoard() {
+  // This for loop creates the rows
+  for (var i = 0; i < rowCol; i++) {
+    var rowStr = "row_" + i; //creates custom id for each <tr>
+
+    //Appends text to create a tr in the html file
+    $("#view_board").append("<tr id=" + rowStr + "></tr>");
+    //This for loops creates the cells within the rows
+    for (var j = 0; j < rowCol; j++) {
+      var colStr = "" + i + j; //creates custom id for each td
+
+      //appends text to create a td in the html file
+      $("#"+rowStr).append("<td id=" + colStr + ">" + i + j + "</td>");
+    }
+
+    //appends text to close tr
+    $("#view_board").append("</tr>");
+  }
+
+  // Shows the current number of torpedoes
+  $("#torpedoes").text("Torpedoes remaining: " + (maxTorpedoes - numTorpedoes));
+}
+
+// TODO handle changing color based on hit/miss
+function changeColor(str) {
+  $(str)
+}
---
+++ model.js
@@ -0,0 +1,227 @@
+var SHIP = 1; // Global constant
+var rowCol = 10; // The number of rows/columns
+var numTorpedoes = 0; // The number of torpedoes that have been fired
+var maxTorpedoes = 50; //The Maximum torpedoes before you lose
+var numShips = 0; // The number of ships in the game
+var numHits = 0;
+var maxHits = 0;
+var arrayOfShips = [];
+var shipObject = {};
+var shipCoords = [];
+
+
+// The backend game board (will be a 10x10 matrix)
+var board = [ [],
+              [],
+              [],
+              [],
+              [],
+              [],
+              [],
+              [],
+              [],
+              [] ];
+
+// Populates board so there are ten arrays of ten with the value 0 using two
+// for loops
+function makeModelBoard() {
+  for (var i = 0; i < rowCol; i++) {
+    for (var j = 0; j < rowCol; j++) {
+      board[i].push(0);
+    }
+  }
+}
+
+function placeShipOfLength(length) {
+  var attemptsLeft = 100; // Make sure we don't loop infinitely
+  shipCoords = [];
+  while (attemptsLeft > 0) {
+  	var placeHorizontal = (Math.floor(Math.random() * 2) !== 0);
+		var x = Math.floor(Math.random() * rowCol);
+  	var y = Math.floor(Math.random() * rowCol);
+
+  	if (mayPlaceShip(x, y, length, placeHorizontal)) {
+  		var stop = (placeHorizontal ? x : y) + length;
+
+  		while (true) {
+  			board[x][y] = SHIP;
+        numShips ++;
+        maxHits ++;
+        shipCoords = shipCoords.concat([x + "," + y]);
+        // arrayOfShips = arrayOfShips.concat(y)
+  			// Uncomment to step through and see ships being added in real time.
+   			// showShips();
+
+  			if (placeHorizontal) {
+  				x++;
+  				if (x == stop) break;
+  			}
+  			else {
+  				y++;
+  				if (y == stop) break;
+  			}
+  		}
+  		return;
+  	}
+    shipObject = {length, placeHorizontal, shipCoords};
+    arrayOfShips = arrayOfShips.concat(shipObject);
+    console.log(arrayOfShips);
+  	attemptsLeft--;
+  }
+}
+
+function placeShipOfLengthAndOrientation(length, isHorizontal) {
+  var attemptsLeft = 100; // Make sure we don't loop infinitely
+  shipCoords = [];
+  while (attemptsLeft > 0) {
+		var x = Math.floor(Math.random() * rowCol);
+  	var y = Math.floor(Math.random() * rowCol);
+  	if (mayPlaceShip(x, y, length, isHorizontal)) {
+  		var stop = (isHorizontal ? x : y) + length;
+
+  		while (true) {
+  			board[x][y] = SHIP;
+        numShips ++;
+        maxHits ++;
+        shipCoords = shipCoords.concat([x + "," + y]);
+        // arrayOfShips = arrayOfShips.concat(y)
+
+  			// Uncomment below to step through and see ships being added in real time.
+   			// showShips();
+
+  			if (isHorizontal) {
+  				x++;
+  				if (x == stop) break;
+  			}
+  			else {
+  				y++;
+  				if (y == stop) break;
+  			}
+  		}
+  		return;
+  	}
+    shipObject = {length, isHorizontal, shipCoords};
+    arrayOfShips = arrayOfShips.concat(shipObject);
+    console.log(arrayOfShips);
+  	attemptsLeft--;
+  }
+}
+
+function mayPlaceShip(x, y, length, horizontal) {
+	var stop = (horizontal ? x : y) + length;
+
+	while (true) {
+		if (!mayPlace(x, y)) {
+			return false; // If we find an invalid coordinate, we're done.
+		}
+
+		if (horizontal) {
+			x++;
+			if (x === stop) break;
+		}
+		else {
+			y++;
+			if (y === stop) break;
+		}
+	}
+
+	return true;
+}
+
+// This function checks to see if we are allowed to place part of a ship
+// at the cell located at (x, y). It returns false if: (a) the given coordinates
+// are out-of-bounds, (b) part of an existing ship already spans the cell, or (c)
+// part of an existing ship spans an adjacent cell (including those diagonally-adjacent).
+function mayPlace(x, y) {
+	// Coordinates are out-of-bounds
+	if (!coordinatesAreInBounds(x, y)) {
+		return false;
+	}
+
+	// Cell already occupied
+	if (board[x][y] === SHIP) {
+		return false;
+	}
+
+	// At least one adjacent cell is occupied
+	// return (neighboringCells(x, y).filter(function(cell){return cell === SHIP}).length == 0);
+	var neighbors = neighboringCells(x, y);
+	return (neighbors.reduce(function(sum, value){return sum + value;}) === 0);
+}
+
+function neighboringCells(x, y) {
+	var neighbors = [];
+
+	var onLeftEdge = (x === 0);
+	var onRightEdge = (x === (board.length - 1));
+	var onTopEdge = (y === 0);
+	var onBottomEdge = (y === (board[x].length - 1));
+
+		// Row above
+	if (!onTopEdge) {
+		// Top-left
+		if (!onLeftEdge) neighbors.push(board[x - 1][y - 1]);
+
+		// Top
+		neighbors.push(board[x][y - 1]);
+
+		// Top-right
+		if (!onRightEdge) neighbors.push(board[x + 1][y - 1]);
+	}
+
+	// Left-adjacent
+	if (!onLeftEdge) neighbors.push(board[x - 1][y]);
+
+	// Right-adjacent
+	if (!onRightEdge) neighbors.push(board[x + 1][y]);
+
+	// Row below
+	if (!onBottomEdge) {
+		// Bottom-left
+		if (!onLeftEdge) neighbors.push(board[x - 1][y + 1]);
+
+		// Bottom
+		neighbors.push(board[x][y + 1]);
+
+		// Bottom-right
+		if (!onRightEdge) neighbors.push(board[x + 1][y + 1]);
+	}
+
+	return neighbors;
+}
+
+function coordinatesAreInBounds(x, y) {
+	if (x < 0 || y < 0) return false;
+
+	return (x < board.length && y < board[x].length);
+}
+
+//This function finds an element in the matrix, and returns its value
+//controller uses this to access the array in the model
+function findOnBoard(num) {
+  var row = Math.floor(num / 10);
+  var col = num % 10;
+  return board[row][col];
+}
+
+// Increments the number of hits and checks if user has won (i.e., the number
+// of hits is equal to maxHits)
+function addHitAndCheckWin() {
+  numHits++;
+  console.log(numHits);
+  return numHits >= maxHits;
+}
+
+//Increases number of torpedoes by 1 and checks if the user lost.
+function addTorpedoAndCheckLose() {
+  numTorpedoes++;
+  return numTorpedoes >= maxTorpedoes;
+}
+
+
+function showBoard() {
+  console.log("   " + 0 + " " + 1 + " " + 2 + " " + 3 + " " + 4 + " " + 5 + " " + 6 + " " + 7 + " " + 8 + " " + 9);
+  for (var i = 0; i < rowCol; i++) {
+    console.log(i + ": " + board[i][0] + " " + board[i][1] + " " + board[i][2] + " " + board[i][3] + " " + board[i][4] + " " + board[i][5] + " " + board[i][6] + " " + board[i][7] + " " + board[i][8] + " " + board[i][9]);
+  }
+}
